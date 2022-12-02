5f43460b66665ed9b18b2d7b55a8d6711eca366f
XavierJ69 <63133001+XavierJ69@users.noreply.github.com>
2022-12-02 14:31:54 -0600

    Made country models and finished female shoe data file

--- FastFitProject/Data/MaleShoeSizeData.cs
+++ FastFitProject/Data/MaleShoeSizeData.cs
@@ -1,6 +1,17 @@
-﻿namespace FastFit_Final_Project.Data
+﻿using Fast_Fit_Final_Project.Model;
+using Fast_Fit_Final_Project.Models;
+using System.Collections.Generic;
+
+namespace FastFit_Final_Project.Data
 {
     public class MaleShoeSizeData
     {
+        static private string DATA_FILE = "Data/MaleShoeData.csv";
+
+        static bool IsDataLoaded;
+
+        static List<Country> countries;
+
+        static private List<MaleShoe> Value = new List<MaleShoe>();
     }
 }
---
+++ FastFitProject/Models/Eu.cs
@@ -0,0 +1,10 @@
+﻿using Fast_Fit_Final_Project.Model;
+using Fast_Fit_Final_Project.Models;
+
+namespace FastFitProject.Models
+{
+    public class Eu : FemaleShoe
+    {
+        public Eu(string value) : base(value) { }
+    }
+}
