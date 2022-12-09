dee604b4b1129d26edd30927960f2837391a5b8c
XavierJ69 <63133001+XavierJ69@users.noreply.github.com>
2022-12-09 15:49:41 -0600

    Fixed conflicts and validation

--- FastFitProject/ViewModels/MemberDetailViewModel.cs
+++ FastFitProject/ViewModels/MemberDetailViewModel.cs
@@ -12,7 +12,7 @@ namespace FastFitProject.ViewModels
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Name is required.")]
-        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 50 characters")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Name must be between 1 and 50 characters")]
         public string Name { get; set; }
         public int Age { get; set; }
 
--- FastFitProject/ViewModels/MembersViewModel.cs
+++ FastFitProject/ViewModels/MembersViewModel.cs
@@ -10,7 +10,7 @@ namespace Fast_Fit_Final_Project.ViewModels
     {
 
         [Required(ErrorMessage = "Name is required.")]
-        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 50 characters")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Name must be between 1 and 50 characters")]
         public string Name { get; set; }
 
         public int Age { get; set; }
